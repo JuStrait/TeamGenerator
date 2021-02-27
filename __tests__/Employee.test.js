487def15c55249e137235ae942d7aa5efb624ca0
JuStrait <justin.l.strait@gmail.com>
2021-02-27 15:04:13 -0600

    final test push

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,19 +4,19 @@ const Employee = require('../lib/Employee.js');
 const name = "test";
 const email = "test@email.com";
 const id = "id";
-const employee = new Employee(1, 'test', 'test@mail.com', 'id',);
+const employee = new Employee(name, id, email);
 
 describe('Employee class', () => {
     test('class created', () =>{
         expect(employee).toBeTruthy();
     });
     test("verify role", () => {
-        expect(employee.getRole()).toEqual('id');
+        expect(employee.getRole()).toEqual('Employee');
     });
     test("verify id", () => {
-        expect(employee.getId()).toEqual('test');
+        expect(employee.getId()).toEqual(id);
     });
     test("verify email", () => {
-        expect(employee.getEmail()).toEqual('test@mail.com');
+        expect(employee.getEmail()).toEqual('test@email.com');
     });
 });
